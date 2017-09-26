0d3fa834a75ce5256b0b8e2fa6e2affeb837d3c2
FareedQ <QuraishiFareed@gmail.com>
2017-09-26 12:09:00 -0400

    Adding Bugs to the Scene

--- ARniegeddon/GameViewController.swift
+++ ARniegeddon/GameViewController.swift
@@ -97,4 +97,11 @@ extension GameViewController: ARSKViewDelegate {
                           options: [.resetTracking,
                                     .removeExistingAnchors])
   }
+  
+  func view(_ view: ARSKView,
+            nodeFor anchor: ARAnchor) -> SKNode? {
+    let bug = SKSpriteNode(imageNamed: "bug")
+    bug.name = "bug"
+    return bug
+  }
 }
