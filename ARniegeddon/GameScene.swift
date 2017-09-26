4f6e7c4251f8558f36e57f97fcb245948e11b8dc
FareedQ <QuraishiFareed@gmail.com>
2017-09-26 12:15:27 -0400

    Shooting Bugs

--- ARniegeddon/GameScene.swift
+++ ARniegeddon/GameScene.swift
@@ -33,6 +33,7 @@ import ARKit
 class GameScene: SKScene {
   
   var isWorldSetUp = false
+  var sight: SKSpriteNode!
   
   var sceneView: ARSKView {
     return view as! ARSKView
@@ -69,6 +70,34 @@ class GameScene: SKScene {
         bug.color = .black
         bug.colorBlendFactor = blendFactor
       }
+    }
+  }
+  
+  override func didMove(to view: SKView) {
+    sight = SKSpriteNode(imageNamed: "sight")
+    addChild(sight)
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>,
+                             with event: UIEvent?) {
+    let location = sight.position
+    let hitNodes = nodes(at: location)
+    var hitBug: SKNode?
+    for node in hitNodes {
+      if node.name == "bug" {
+        hitBug = node
+        break
+      }
+    }
+    run(Sounds.fire)
+    if let hitBug = hitBug,
+      let anchor = sceneView.anchor(for: hitBug) {
+      let action = SKAction.run {
+        self.sceneView.session.remove(anchor: anchor)
+      }
+      let group = SKAction.group([Sounds.hit, action])
+      let sequence = [SKAction.wait(forDuration: 0.3), group]
+      hitBug.run(SKAction.sequence(sequence))
     }
   }
   
